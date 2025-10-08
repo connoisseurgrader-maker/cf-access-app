3f564160c504421382bb7658ba0b96c42cd2ba30
Ebanezer Victor <ebanezerv@gmail.com>
2025-10-08 18:54:33 +1100

    docs: add env.example.sh (no secrets)

---
+++ env.example.sh
@@ -0,0 +1,3 @@
+export DATABASE_URL="postgresql+psycopg2://cfapp:CHANGE_ME@localhost:5432/cfapp"
+export CF_TEAM_DOMAIN="connoisseurgrader.cloudflareaccess.com"
+export CF_ACCESS_AUD="13af5e6063b2c257051ef5e7aaf4c6b4e6d438c15e4c634b7a6bf1f9d77e8f7e"
